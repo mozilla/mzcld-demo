c4380b2d9562e102ba9d29fdf1f431f5976fa87b
Will Kahn-Greene <willkg@mozilla.com>
2025-06-05 12:50:12 -0400

    Write tests for all routes
    This adds tests for all the routes currently defined except for chain.
    I think we need to rethink chain to work in a test, local dev, stage,
    and prod environments so I added a FIXME for that.

--- load-tests/k6-script.js
+++ load-tests/k6-script.js
@@ -3,7 +3,7 @@ import http from 'k6/http';
 export default function() {
   var hostname = __ENV.LOAD_TEST_HOSTNAME || "localhost:8000"
   var server_list = [hostname]
-  var endpoint_list = ["/", "/io_task", "/cpu_task", "/random_sleep", "/random_status", "/chain", "/error_test"]
+  var endpoint_list = ["/", "/io_task", "/cpu_task", "/random_sleep", "/random_status", "/chain", "/missing_page_test"]
   server_list.forEach(function(server) {
     endpoint_list.forEach(function(endpoint) {
       http.get(`http://${server}${endpoint}`);
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import pytest
 
 from app.main import app
 
@@ -6,7 +7,47 @@ from app.main import app
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_read_item():
+    response = client.get("/items/5")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 5, "q": None}
+
+
+def test_io_task():
+    response = client.get("/io_task")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "IO bound task finish!"}
+
+
+def test_cpu_task():
+    response = client.get("/cpu_task")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "CPU bound task finish!"}
+
+
+def test_random_status():
+    response = client.get("/random_status")
+    assert response.json() == {"path": "/random_status"}
+
+
+def test_random_sleep():
+    response = client.get("/random_sleep")
+    assert response.status_code == 200
+    assert response.json() == {"path": "/random_sleep"}
+
+
+def test_unhandled_exception():
+    with pytest.raises(ValueError):
+        client.get("/unhandled_exception")
+
+
+def test_http_exception():
+    response = client.get("/http_exception")
+    assert response.status_code == 500
+    assert response.json() == {"detail": "http exception"}
